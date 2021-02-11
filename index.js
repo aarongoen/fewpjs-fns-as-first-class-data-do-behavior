ad6c077a51998e01ed86b872caa2ea820f2c82a9
aarongoen <aarongoen2.0@gmail.com>
2021-02-11 11:41:44 -0500

    commit tree d851003b26f478819595948d4fcf88abf7882f30

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+let time = '15:31'
+
+function greet(time) {
+  let splitTime = time.split(':')[0];
+  if (splitTime < 12) return 'Good Morning';
+  if ((splitTime > 12) && (splitTime < 17)) return 'Good Afternoon';
+  return 'Good Evening';
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById('greeting').innerText = str;
+}
